023500cb2a2ea441ac5a3cef218b049ad0b63018
Rodolfo Gomes <rodolfoaguiargomes@gmail.com>
2018-04-23 19:14:19 -0300

    Criando a conexão com o banco de dados

---
+++ src/BD/ConnectionFactory.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BD;
+
+/**
+ *
+ * @author Rodolfo Gomes
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import sun.applet.Main;
+
+public class ConnectionFactory {
+    
+    /**
+     *
+     * Método para gerar conexão com o banco de dados.
+     * @throws java.sql.SQLException
+     */
+    
+      /* Configura os parâmetros da conexão */
+           private static final String  URL = "jdbc:mysql:ip:porta/schema";
+           private static final String  USERNAME = "projeto"; 
+           private static final String  PASSWORD = "projeto";
+    
+    
+    public static Connection getConnection() throws SQLException{
+        try {
+            /* Tenta se conectar */
+            return  DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+                throw new RuntimeException("ERRO: ",ex);
+        }
+    }
+    
+     
+}
+  
---
+++ src/unirecife/UniRecife.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unirecife;
+
+import BD.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Rodolfo Gomes
+ */
+public class UniRecife {
+
+    public static void main(String[] args) throws SQLException {
+     
+        Connection con = ConnectionFactory.getConnection();
+        
+    }
+    
+}
