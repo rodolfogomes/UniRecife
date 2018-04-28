f42979e2f138d240094101d2436bd5cdd76c7b42
Rodolfo Gomes <rodolfoaguiargomes@gmail.com>
2018-04-28 11:44:31 -0300

    ajustes nos arquivos de conexão.

--- src/unirecife/UniRecife.java
+++ src/unirecife/UniRecife.java
@@ -18,6 +18,8 @@ public class UniRecife {
     public static void main(String[] args) throws SQLException {
      
         Connection con = ConnectionFactory.getConnection();
+        System.out.println("Conexão funciona!");
+        con.close();
         
     }
     
