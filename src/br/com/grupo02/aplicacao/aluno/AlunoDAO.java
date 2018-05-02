fc7ac4742dc8234f93bf5fde8f22d518980ead7c
Daniel Medeiros <danielmedeiros52@gmail.com>
2018-05-02 14:45:59 -0300

    implentando o DAO

---
+++ src/br/com/grupo02/aplicacao/aluno/AlunoDAO.java
@@ -0,0 +1,24 @@
+
+package br.com.grupo02.aplicacao.aluno;
+
+import br.com.grupo02.erro.ConexaoException;
+import br.com.grupo02.interfaces.GerenciadorConexao;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Daniel
+ */
+public class  AlunoDAO implements GerenciadorConexao  {
+
+    @Override
+    public Connection conectar() throws ConexaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void desconectar(Connection c) throws ConexaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
